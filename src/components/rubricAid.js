dca7871480827d93a9cf57703d44b88e1f23f80c
justinlcochran <justinlcochran@gmail.com>
2023-02-25 12:48:10 -0700

    rubriAid updates

--- src/components/rubricAid.js
+++ src/components/rubricAid.js
@@ -3,7 +3,7 @@ import React from 'react';
 function RubricAid({bottom, middle, top}) {
     return (
         <div className={'col-span-5 grid grid-cols-5 bg-slate-100'}>
-            <div className={'col-span-1 text-black text-xs my-auto py-2'}><p>{bottom}</p></div>
+            <div className={'col-span-1 text-black text-xs my-auto py-2'}><p>{(bottom) ? (bottom.includes("\n")) ? bottom.split('\n').map(item => <p>{item}</p>) : bottom : bottom}</p></div>
             <div className={'col-span-1'}></div>
             <div className={'col-span-1 text-black text-xs my-auto mx-2 p-2'}><p>{(middle) ? (middle.includes("\n")) ? middle.split('\n').map(item => <p>{item}</p>) : middle : middle}</p></div>
             <div className={'col-span-1'}></div>
