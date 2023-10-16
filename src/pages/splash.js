6587ca56819b26c61b3383dc0b3b48482e5ea176
justinlcochran <justinlcochran@gmail.com>
2023-10-16 10:45:10 -0600

    add teacher nav

--- src/pages/splash.js
+++ src/pages/splash.js
@@ -20,7 +20,7 @@ function Splash() {
         <div className={'h-screen flex bg-gray-200'}>
             <button className={'p-6 bg-blue-500 text-white text-6xl select-none rounded-lg mx-auto my-auto border-white border-2 shadow-xl font-sans'} onClick={previewNav}>Teacher Dashboard</button>
 
-            <button className={'p-6 bg-blue-500 text-white text-6xl select-none rounded-lg mx-auto my-auto border-white border-2 shadow-xl font-sans'} onClick={previewNav}>Rubric Preview</button>
+            <button className={'p-6 bg-blue-500 text-white text-6xl select-none rounded-lg mx-auto my-auto border-white border-2 shadow-xl font-sans'} onClick={teacherNav}>Rubric Preview</button>
         </div>
     );
 }
